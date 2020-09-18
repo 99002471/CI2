1251abcb9e8af5d8478e1fa3d68202c7bcddf87f
99002471 <71489857+99002471@users.noreply.github.com>
2020-09-19 01:31:13 +0530

    Update calculator_operations.h

--- miniproject/implementation/inc/calculator_operations.h
+++ miniproject/implementation/inc/calculator_operations.h
@@ -8,15 +8,9 @@
 #include <stdio_ext.h>
 
 int add(int operand1, int operand2);
-
-
 int subtract(int operand1, int operand2);
-
-
 int multiply(int operand1, int operand2);
-
 int divide(int operand1, int operand2);
-
 int prime(int operand1);
 int pos(int operand1);
 int neg(int operand1);
@@ -24,6 +18,7 @@ int iszero(int operand1);
 int modu(int operand1, int operand2);
 int eve(int operand1);
 int odd(int operand1);
+int perc(int operand1, int operand2);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
 
